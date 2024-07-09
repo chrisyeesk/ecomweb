18485cbe93ca95b556670db25b2b4cab1d379ddf
Yao Xian Phang <80228647+yaoxianphang@users.noreply.github.com>
2024-07-09 18:59:13 +1000

    modified the view of contact page

--- frontend/app/(customer)/contact/page.tsx
+++ frontend/app/(customer)/contact/page.tsx
@@ -1,11 +1,10 @@
-import Footer from "../components/footer";
 import EnquiryForm from "../components/enquiry-form";
 
 export default function CustomerSupport() {
   return (
     <>
-      <main className="pt-16 mx-auto w-[100vw] xl:w-[1500px]">
-        <div className="max-w-5xl mx-auto my-10 p-10 border border-gray-300 shadow-lg flex flex-col lg:flex-row">
+      <main className="pt-16 mx-auto w-full max-w-[1500px] px-4 sm:px-6 lg:px-8">
+        <div className="max-w-5xl mx-auto my-20 p-4 sm:p-6 md:p-10 border border-gray-300 shadow-lg flex flex-col lg:flex-row">
           {/* Top Section (on mobile) / Left Section (on desktop) */}
           <div className="w-full lg:w-1/3 lg:pr-8 mb-6 lg:mb-0">
             <h2 className="text-4xl font-bold mb-6">Contact.</h2>
@@ -57,7 +56,6 @@ export default function CustomerSupport() {
           </div>
         </div>
       </main>
-      <Footer />
     </>
   );
 }
